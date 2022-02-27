0b2af2dc4429988ae0e1bc0b93911704d3f0bd71
Miroslav6 <miroslav.nedev1@gmail.com>
2022-02-27 14:53:07 +0200

    Remove redundant files
    Remove redundant files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import Layout from './pages/layout'
-import Table from './pages/table'
 import NoPage from './pages/NoPage'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
@@ -21,7 +20,6 @@ ReactDOM.render(
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
-              <Route path="table" element={<Table />} />
               <Route path="*" element={<NoPage />} />
             </Route>
           </Routes>
--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,36 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
-import Logo from '../Files/Images/logo.png'
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
         <>
-            <header className='sticky-top'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col col-sm-1'>
-                            <a href='/' title='logo'>
-                                <img src={Logo} alt="Logo" className="img-fluid" />
-                            </a>
-                        </div>
-                        <div className='col col-sm-8'>
-                            <nav className='h-100 d-flex justify-content-center'>
-                                <ul className='align-items-center'>
-                                    <li>
-                                        <Link to="/">Home</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/table">Tables</Link>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <div className='col col-sm-3 d-flex align-items-center justify-content-end'>
-                            <a href='tel:+359883378679' className='hd-phone'><i className="bi bi-telephone"></i> +359 883378679</a>
-                        </div>
-                    </div>
-                </div>
-            </header>
-            <main>
+             <main>
                 <Outlet />
             </main>
         </>
