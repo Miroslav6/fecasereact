5886fcdc2b61f53cdd712b91e422d424dc1a96d2
Miroslav6 <miroslav.nedev1@gmail.com>
2022-02-27 10:51:10 +0200

    Add selected row functionality
    Add selected row functionality

--- src/pages/table.jsx
+++ src/pages/table.jsx
@@ -8,31 +8,45 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import api from '../api/posts'
+import $ from "jquery";
 
-const TableComponent = () => {
+const TableComponent = (props) => {
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
-    
+    const [isChecked, setIsChecked] = useState(false);
+
     useEffect(() => {
 
         const fetchData = async () => {
             setLoading(true);
             try {
                 const { data: response } = await api.get('/users');
-                console.log(response);
+                const newData = response.map(obj=> ({ ...obj, isChecked: 'false' }))
+                console.log(newData);
                 setData(response);
-                
+
             } catch (error) {
                 console.error(error.message);
             }
             setLoading(false);
         }
-
+        
         fetchData();
-    }, []);
-    
-    const tableHeading = ['ID', 'Name', 'Username', 'Email', 'City'];
 
+
+    }, []);
+
+    const tableHeading = ['', 'ID', 'Name', 'Username', 'Email', 'City'];
+    const handleCheckboxChange = (e, row) => {
+       setIsChecked(e);
+        if(e){
+            $(`#row-${row.id}`).addClass('selected-row');
+        }
+        else {
+            $(`#row-${row.id}`).removeClass('selected-row');
+        }
+        
+    };
     return (
         <div className='container mt-5'>
             {!loading && (
@@ -48,12 +62,25 @@ const TableComponent = () => {
                         </TableHead>
 
                         <TableBody>
-                            {data.map((row) => (
+                            {data.map((row, index) => (
                                 <TableRow
+                                    id={`row-${row.id}`}
+                                    // className={isChecked ? 'best' : ''}
                                     key={row.name}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
-                                    <TableCell component="th" scope="row">
+                                    <TableCell component="td" scope="row">
+                                        <input
+                                            id={`custom-checkbox-${data.id}`}
+                                            type="checkbox"
+                                            // checked={isChecked}
+                                            onChange={(e) => handleCheckboxChange(e.target.checked, row)}
+                                            // onChange={handleCheckboxChange}
+                                            // checked={isChecked}
+                                            
+                                        />
+                                    </TableCell>
+                                    <TableCell component="td" scope="row">
                                         {row.id}
                                     </TableCell>
                                     <TableCell align="right">{row.name}</TableCell>
